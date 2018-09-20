fd4ec0be9e3699665a9f984848a06b14bb997213
elhassen <hacenselahy@gmail.com>
2018-09-20 17:04:52 +0000

    Access-Alow-Origin

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(bodyParser.json({limit: '50mb', extended: true}))
 
 app.use(express.static(path.join(__dirname, 'dist/postsNg')))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
+
 app.get('/api/posts', posts)
 
 app.get('/api/posts/:id', posts)
